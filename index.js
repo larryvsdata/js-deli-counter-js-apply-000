012acdb8cc712de3bacf0afca09c06b59613da3e
larryvsdata <ermanbekaroglu@gmail.com>
2018-08-19 04:07:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var katzDeliLine=[]
-var =0;
+
 
 function takeANumber(currentLine,name){
   
   katzDeliLine[currentLine]=name;
-  currentLine+=1;
+  
   return "Welcome, "+name+". You are number "+ currentLine +" in line.";
 }
