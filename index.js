474f70540e5d12ad4a29190321e8a4b882595ed5
larryvsdata <ermanbekaroglu@gmail.com>
2018-08-19 04:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function takeANumber(katzDeliLine,name){
   
   return "Welcome, "+name+". You are number "+ katzDeliLine.length +" in line.";
 }
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length===0){
+    return ""
+  }
+  
+}
