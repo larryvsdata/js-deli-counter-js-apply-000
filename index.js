960949ecf1672927c7c0097c26fcd482471af702
larryvsdata <ermanbekaroglu@gmail.com>
2018-08-19 04:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var katzDeliLine=[]
+var currentLine=0;
 
-
-function takeANumber(currentLine,name){
+function takeANumber(name){
+  
   katzDeliLine[currentLine]=name;
+  currentLine+=1;
   return "Welcome, "+name+". You are number "+ currentLine +" in line.";
 }
